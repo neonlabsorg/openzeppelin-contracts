df65c6d03eff12b263694dc07e7a02f2b6dfef3d
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-05-13 16:53:32 +0300

    Increase the transfer to eip deployer

--- test/helpers/eip1820.js
+++ test/helpers/eip1820.js
@@ -1,10 +1,9 @@
 const { singletons } = require('@openzeppelin/test-helpers');
-const { setSingletonsConfig, getSingletonsConfig } = require('@openzeppelin/test-helpers/src/config/singletons')
-const {web3} = require("@openzeppelin/test-helpers/src/setup");
-const {ERC1820_REGISTRY_ADDRESS} = require("@openzeppelin/test-helpers/src/data");
-const send = require("@openzeppelin/test-helpers/src/send");
-const ether = require("@openzeppelin/test-helpers/src/ether");
-
+const { setSingletonsConfig, getSingletonsConfig } = require('@openzeppelin/test-helpers/src/config/singletons');
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
+const { ERC1820_REGISTRY_ADDRESS } = require('@openzeppelin/test-helpers/src/data');
+const send = require('@openzeppelin/test-helpers/src/send');
+const ether = require('@openzeppelin/test-helpers/src/ether');
 
 async function NeonERC1820Registry (funder) {
   const code = await web3.eth.getCode(ERC1820_REGISTRY_ADDRESS);
@@ -14,12 +13,12 @@ async function NeonERC1820Registry (funder) {
     abstraction: config.abstraction,
     defaultGas: 1e7,
     defaultSender: config.defaultSender,
-  })
-  
+  });
+
   if (code.length <= '0x0'.length) {
-    await send.ether(funder, '0xa990077c3205cbDf861e17Fa532eeB069cE9fF96', ether('10'));
+    await send.ether(funder, '0xa990077c3205cbDf861e17Fa532eeB069cE9fF96', ether('81'));
   }
-  
+
   return await singletons.ERC1820Registry(funder);
 }
 
